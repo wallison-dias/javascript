397c8aebb8b986248016c6cde8bc7a74ef0dcb31
Wallison Dias <110568122+wallisonvsdias@users.noreply.github.com>
2024-06-10 15:27:27 -0300

    Ajustes finais no exercício

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,23 +1,32 @@
 function contar() {
-    var txtini = document.getElementById("iini")
-    var txtfim = document.getElementById("ifim")
-    var txtpas = document.getElementById("ipas")
-    var ini = Number(txtini.value)
-    var fim = Number(txtfim.value)
-    var pas = Number(txtpas.value)
-    var res = document.getElementsByTagName("p")[0]
+    let txtini = document.getElementById("iini")
+    let txtfim = document.getElementById("ifim")
+    let txtpas = document.getElementById("ipas")
+    let i = Number(txtini.value)
+    let f = Number(txtfim.value)
+    let p = Number(txtpas.value)
+    let res = document.getElementsByTagName("p")[0]
 
     res.innerHTML = "" // Para esvaziar o parágrafo sempre que clicar no botão
-    if (pas <= 0) {
-        window.alert("[ERRO] O passo tem que ser um número positivo. Substituímos por 1")
-        for (ini; ini <= fim; ini += 1) {
-            res.innerHTML += `${ini} \u{1F449}`
-        }
-        res.innerHTML += "\u{1F3C1}"
+    if (txtini.value.length == 0 || txtfim.value.length == 0 || txtpas.value.length == 0) {
+        alert("[ERRO] Faltam dados!")     
+        res.innerHTML = "Impossível contar!"   
     } else {
-        for (ini; ini <= fim; ini += pas) {
-            res.innerHTML += `${ini} \u{1F449}`
+        if (p <= 0) {
+            window.alert("[ERRO] O passo tem que ser um número positivo. Substituímos por 1")
+            p = 1
         }
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            } 
+        } else {
+            // Contagem regressiva            
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        } 
         res.innerHTML += "\u{1F3C1}"
-    }      
+    }
 }
