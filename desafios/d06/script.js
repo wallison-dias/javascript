8a1572f562d2edcc99c061b444714006517a4104
Wallison Dias <110568122+wallisonvsdias@users.noreply.github.com>
2024-07-03 18:22:05 -0300

    Desafios 5, 6 e 7

--- desafios/d06/script.js
+++ desafios/d06/script.js
@@ -1,11 +1,9 @@
-function clickButton1() {
-    window.alert("Você clicou no 1º botão")
-}
-
-function clickButton2() {
-    window.alert("Você clicou no 2º botão")
-}
-
-function clickButton3() {
-    window.alert("Você clicou no 3º botão")
+function convert() {
+    let c = window.prompt("Digite uma temperatura em °C (Celsius)")
+    let k = Number(c) + 273.15
+    let f = Number(c) * 9/5 + 32
+    let res = document.getElementsByTagName("p")[0]
+    res.innerHTML = `A temperatura de ${c}°C, corresponde a...`
+    res.innerHTML += `<p>${k}°K (Kelvin)</p>`
+    res.innerHTML += `<p>${f}°F (Fahrenheit)</p>`
 }
--- desafios/d07/script.js
+++ desafios/d07/script.js
@@ -1,11 +1,15 @@
-function clickButton1() {
-    window.alert("Você clicou no 1º botão")
+function askDolar() {
+    var exchangeRateTEST = window.prompt("Antes de mais nada. Quanto está a cotação do dólar agora?")
+    var exchangeRate = Number(exchangeRateTEST)
+    return Number(exchangeRate)
 }
 
-function clickButton2() {
-    window.alert("Você clicou no 2º botão")
-}
-
-function clickButton3() {
-    window.alert("Você clicou no 3º botão")
+function convert(exchangeRate) {
+    let r = window.prompt("Quantos R$ você tem?")
+    let us = Number(r) * Number(exchangeRate)
+    let res = document.querySelector("p")
+    res.innerHTML = `Você tem US$${us}`
+    // window.alert(typeof Number(r))
+    // window.alert(typeof Number(exchangeRate))
+    // window.alert(typeof us)
 }
