b80795091214cb846910306841b8270da7529da4
Wallison Dias <110568122+wallisonvsdias@users.noreply.github.com>
2024-07-09 11:00:35 -0300

    Desafios 8 e 9 concluídos

--- desafios/d08/script.js
+++ desafios/d08/script.js
@@ -1,11 +1,12 @@
-function clickButton1() {
-    window.alert("Você clicou no 1º botão")
-}
+function discount() {
+    let product = window.prompt("Qual é o produto que você está comprando?")
+    let price = window.prompt(`Qual o preço do(a) ${product}?`)
+    let discount = 0.1 * price
+    let finalprice = price - discount
+    let res = document.querySelector("p")
+    res.innerHTML = `<strong>Calculando desconto de 10% para ${product}</strong>`
+    res.innerHTML += `<p>O preço original era ${price}.</p>`
+    res.innerHTML += `<p>Você acaba de ganhar R$ ${discount} de desconto (-10%).</p>`
+    res.innerHTML += `<p>No fim, você vai pagar R$ ${finalprice} no(a) ${product}.</p>`
 
-function clickButton2() {
-    window.alert("Você clicou no 2º botão")
-}
-
-function clickButton3() {
-    window.alert("Você clicou no 3º botão")
 }
--- desafios/d09/script.js
+++ desafios/d09/script.js
@@ -1,11 +1,13 @@
-function clickButton1() {
-    window.alert("Você clicou no 1º botão")
-}
+function readjust() {
+    let employee = window.prompt("Qual é o nome do funcionário?")
+    let wage = window.prompt(`Qual é o salário de ${employee}?`)
+    let percentage = window.prompt(`O salário de ${employee} vai ser reajustado em qual porcentagem?`)
+    let readjust = percentage/100 * wage
+    let finalwage = Number(wage) + readjust
+    let res = document.querySelector("p")
+    res.innerHTML = `<strong>${employee} recebeu um aumento salarial!</strong>`
+    res.innerHTML += `<p>O salário era R$ ${wage}.</p>`
+    res.innerHTML += `<p>Com um aumento de ${percentage}%, o salário vai aumentar R$ ${readjust} no próximo mês..</p>`
+    res.innerHTML += `<p>E a partir daí, ${employee} vai passar a ganhar R$ ${finalwage}.</p>`
 
-function clickButton2() {
-    window.alert("Você clicou no 2º botão")
-}
-
-function clickButton3() {
-    window.alert("Você clicou no 3º botão")
 }
